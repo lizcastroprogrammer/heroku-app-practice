76d3dced89ba41397cd5f29211d423b7cf19cb07
lizcastroprogrammer <elizabethjcastro42@gmail.com>
2022-07-15 09:42:53 -0500

    update route names

--- http_server.js
+++ http_server.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const low = require('lowdb');
-const fs = require('lowdb/adapters/FileSync');
-const adapter = new fs('db.json');
+const low = require("lowdb");
+const fs = require("lowdb/adapters/FileSync");
+const adapter = new fs("db.json");
 const db = low(adapter);
-const cors = require('cors');
+const cors = require("cors");
 
 // allow cross-origin resource sharing (CORS)
 app.use(cors());
 
 // data parser - used to parse post data
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // serve static files from public directory
 // -------------------------------------------
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // init the data store
 db.defaults({ users: [] }).write();
@@ -24,32 +24,32 @@ db.defaults({ users: [] }).write();
 let port = process.env.PORT || 3000;
 
 // return all users
-app.get('/data', function (req, res) {
-    res.send(db.get('users').value());
+app.get("/accounts", function (req, res) {
+  res.send(db.get("users").value());
 });
 
 // add user
-app.post('/add', function (req, res) {
-    var user = {
-        'name': req.body.name,
-        'dob': req.body.dob,
-        'email': req.body.email,
-        'username': req.body.username,
-        'password': req.body.password,
-        'phone': req.body.phone,
-        'streetaddress': req.body.streetaddress,
-        'citystatezip': req.body.citystatezip,
-        'latitude': req.body.latitude,
-        'longitude': req.body.longitude,
-        'avatar': req.body.avatar
-    }
-    db.get('users').push(user).write();
-    console.log(db.get('users').value());
-    res.send(db.get('users').value());
+app.post("/accounts", function (req, res) {
+  var user = {
+    name: req.body.name,
+    dob: req.body.dob,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    phone: req.body.phone,
+    streetaddress: req.body.streetaddress,
+    citystatezip: req.body.citystatezip,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    avatar: req.body.avatar,
+  };
+  db.get("users").push(user).write();
+  console.log(db.get("users").value());
+  res.send(db.get("users").value());
 });
 
 // start server
 // -----------------------
 app.listen(port, function () {
-    console.log(`Running on port ${port}`);
+  console.log(`Running on port ${port}`);
 });
